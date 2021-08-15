09f7721edf14c0359b6095516687c6823b511500
thmsnhl <hallo@thmsnhl.de>
2021-08-15 21:04:19 +0200

    try to clean up

--- espressobot/espressobot.ino
+++ espressobot/espressobot.ino
@@ -5,20 +5,27 @@ SevSeg sevseg; //Ein sieben Segment Objekt initialisieren
 Servo grinder; // Servo, der nickt
 
 int servoInitState = 0;
-int pos = 120; // variable to store the servo position
 
 long duration = 0; // variable to store the value coming from the sensor
 int potPin = 2;    // select the input pin for the potentiometer
 long potValue = 0;
 int durationInSeconds = 5;
 
-void initServo(Servo &theServo)
+int ledState = LOW;
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0; // will store last time LED was updated
+
+void initServo()
 {
   grinder.write(servoInitState);
 }
 
 void setup()
 {
+  grinder.attach(1);
+  grinder.write(servoInitState);
+
   byte numDigits = 2;                                              //Hier wird die Anzahl der Ziffern angegeben
   byte digitPins[] = {4, 5, 2, 3};                                 //Die Pins zu den Ziffern werden festgelegt
   byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};               //Die Pins zu den //Segmenten werden festgelegt
@@ -30,17 +37,13 @@ void setup()
 
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
-
-  grinder.attach(1);
-
-  initServo(grinder);
 }
 
 void turnOn()
 {
   digitalWrite(LED_BUILTIN, HIGH);
   grinder.write(120);
-  delay(15);
+  // delay(15);
   // for (pos = 120; pos <= 0; pos -= 2)
   // { // goes from 0 degrees to 180 degrees
   //   // in steps of 1 degree
@@ -53,7 +56,7 @@ void turnOff()
 {
   digitalWrite(LED_BUILTIN, LOW);
   grinder.write(servoInitState);
-  delay(15);
+  // delay(15);
 
   // for (pos = 0; pos <= 120; pos += 2)
   // { // goes from 0 degrees to 180 degrees
@@ -81,14 +84,22 @@ long calcDuration()
 
 void loop()
 {
+  // here is where you'd put code that needs to be running all the time.
+
+  // check to see if it's time to blink the LED; that is, if the difference
+  // between the current time and last time you blinked the LED is bigger than
+  // the interval at which you want to blink the LED.
+  unsigned long currentMillis = millis();
+
+  // Value of the poti
   potValue = analogRead(potPin);
 
+  // Do the math for a range of 5-30 Seconds
   duration = (potValue / 40) * 1000 + 5000;
 
+  // Set the time for the display
   durationInSeconds = duration / 1000;
   // calcDuration();
-
-  // Serial.println(calcDuration());
 
   // ######## ######## ######## ########
 
@@ -104,18 +115,26 @@ void loop()
                              //jedoch nicht dass das Display komplett dunkel ist. Für die Anzeige einer Zahl
                              //ist allein die "sevseg.refreshDisplay();" Zeile verantwortlich
 
-  // ######## ######## ######## ########
-  // turnOn();
+  if (currentMillis - previousMillis >= duration)
+  {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
 
-  // calcDuration();
+    Serial.println("Check");
+    Serial.println(currentMillis);
+    Serial.println(duration);
 
-  // // delay(calcDuration());
-  // // delay(calcDuration());
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW)
+    {
+      grinder.write(0);
+    }
+    else
+    {
+      // turnOff();
+    }
 
-  // turnOff();
-
-  // delay(50);
-
-  // initServo(grinder);
-  // delay(10);
+    // set the LED with the ledState of the variable:
+    digitalWrite(LED_BUILTIN, ledState);
+  }
 }
