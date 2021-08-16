7022d406284f9a9797c3fac6d3f4e1f8436e2a77
thmsnhl <hallo@thmsnhl.de>
2021-08-16 09:16:04 +0200

    translate the README.md

--- espressobot/espressobot.ino
+++ espressobot/espressobot.ino
@@ -1,18 +1,15 @@
-#include "SevSeg.h" //Die vorher hinzugefügte Library laden
+#include "SevSeg.h" // Necessary library for seven segment display
 
-SevSeg sevseg; //Ein sieben Segment Objekt initialisieren
+SevSeg sevseg; // Initialize seven segment display
 
-long duration = 5000; // variable to store the value coming from the sensor
-int potPin = 2;       // select the input pin for the potentiometer
-int relaisPin = A1;   // select the input pin for the potentiometer
-long potValue = 0;
-int durationInSeconds = 5;
+long duration = 5000; // initialize the duration to grind
+long potValue = 0;    // initialize the potentiometer value
+int relayState = LOW; // relayState used to set the LED
 
-// constants won't change. Used here to set a pin number:
-const int ledPin = LED_BUILTIN; // the number of the LED pin
-
-// Variables will change:
-int ledState = LOW; // ledState used to set the LED
+// constants won't change. Used here to set pin numbers:
+const int potPin = 2;           // select the input pin for the potentiometer
+const int relayPin = A1;        // select the input pin for the potentiometer
+const int ledPin = LED_BUILTIN; // (optional for visual feedback) the number of the LED pin
 
 // Generally, you should use "unsigned long" for variables that hold time
 // The value will quickly become too large for an int to store
@@ -23,63 +20,60 @@ void setup()
   Serial.begin(115200);
 
   // set the digital pin as output:
-  pinMode(ledPin, OUTPUT);
-  pinMode(relaisPin, OUTPUT);
+  pinMode(ledPin, OUTPUT); // (optional for visual feedback)
+  pinMode(relayPin, OUTPUT);
 
-  byte numDigits = 2;                                              //Hier wird die Anzahl der Ziffern angegeben
-  byte digitPins[] = {4, 5, 2, 3};                                 //Die Pins zu den Ziffern werden festgelegt
-  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};               //Die Pins zu den //Segmenten werden festgelegt
-  sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins); //In diesem
-                                                                   //Abschnitt kann man nun entweder testen welche Art von Display man besitzt oder
-                                                                   //wenn man es schon weiß angeben ob es sich um ein COMMON_CATHODE oder
-                                                                   //COMMON_ANODE Display handelt. Das Display funktioniert nur wenn die richtige
-                                                                   //Art eingetragen ist, ansonsten werden alle Segmente gleichzeitig leuchten.
+  byte numDigits = 2; // Here we set the number of digits for our seven segment display
+  byte digitPins[] = {4, 5, 2, 3};
+  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
+  bool resistorsOnSegments = false;     // 'false' means resistors are on digit pins
+  byte hardwareConfig = COMMON_CATHODE; // See README.md of SevSeg for options
+  bool updateWithDelays = false;        // Default 'false' is Recommended
+  bool leadingZeros = false;            // Use 'true' if you'd like to keep the leading zeros
+  bool disableDecPoint = true;          // Use 'true' if your decimal point doesn't exist or isn't connected
 
-  sevseg.setBrightness(100); //Hier kann die Helligkeit des Displays angepasst
-                             //werden. In einem Bereich von 0-100 wobei 100 das Hellste ist. 0 bedeutet
-                             //jedoch nicht dass das Display komplett dunkel ist. Für die Anzeige einer Zahl
-                             //ist allein die "sevseg.refreshDisplay();" Zeile verantwortlich
+  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments,
+               updateWithDelays, leadingZeros, disableDecPoint);
+  sevseg.setBrightness(90);
+}
+
+// Convert the duration into seconds to display it
+int durationInSeconds()
+{
+  return duration / 1000;
 }
 
 void loop()
 {
   // here is where you'd put code that needs to be running all the time.
 
-  // Value of the poti
-  potValue = analogRead(potPin);
+  potValue = analogRead(potPin); // Value of the poti
 
-  // Do the math for a range of 5-30 Seconds
-  duration = (potValue / 40) * 1000 + 5000;
+  duration = (potValue / 40) * 1000 + 5000; // Do the math for a range of 5-30 Seconds
 
-  // Set the time for the display
-  durationInSeconds = duration / 1000;
+  sevseg.setNumber(durationInSeconds(), 4); // Set the number to be displayed, the parameter '4' defines where to set the decimal point 4 = no decimal point
 
-  sevseg.setNumber(durationInSeconds, 4); //Hier können wir nun die gewünschte Zahl eintragen.
-  //Wir haben als Beispiel 1234 angegeben. Die Zahl hinter dem Komma steht für den
-  //Punkt hinter einer Ziffer. Hierbei ist 3 der Punkt neben der ersten Ziffer und
-  //0 wäre der Punkt ganz rechts neben der letzten Ziffer. Wenn man keinen Punkt
-  //mit angezeigt haben möcht kann man z.B. 4 angeben.
+  sevseg.refreshDisplay(); // This is what actually displays the number
 
-  unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis(); // Set current time
 
   if (currentMillis - previousMillis >= duration)
   {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
 
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW)
+    previousMillis = currentMillis; // Save the last time you triggered the grinder
+
+    // If the LED is off turn it on and vice-versa:
+    if (relayState == LOW)
     {
-      ledState = HIGH;
+      relayState = HIGH;
     }
     else
     {
-      ledState = LOW;
+      relayState = LOW;
     }
 
-    // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
-    digitalWrite(relaisPin, ledState);
+    digitalWrite(ledPin, relayState); // (optional for visual feedback) set the on-board LED with the relayState of the variable:
+
+    digitalWrite(relayPin, relayState); // toggle the grinder relay
   }
-  sevseg.refreshDisplay(); // Dieser Teil lässt die Nummer auf dem Display
 }
